5249b3f703fd577b37fb9029a45f353a728bebc0
HaroldasZapalskis <HAROLDAS.zapalskis@gmail.com>
2021-04-02 15:57:34 +0300

    Add files via upload

---
+++ precache-manifest.8b0dd06eac5c87b9ea58f0d4a4364d5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6014e3263be94d279b676fc33cd56bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "817247212dbc4947e830",
+    "url": "/static/css/2.b35f0003.chunk.css"
+  },
+  {
+    "revision": "2621fc65130ba3e07385",
+    "url": "/static/css/main.0c26ec99.chunk.css"
+  },
+  {
+    "revision": "817247212dbc4947e830",
+    "url": "/static/js/2.6fb431f3.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/static/js/2.6fb431f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2621fc65130ba3e07385",
+    "url": "/static/js/main.7477d330.chunk.js"
+  },
+  {
+    "revision": "01327198b134424ffc41",
+    "url": "/static/js/runtime-main.baad9156.js"
+  },
+  {
+    "revision": "1c85c342682278f1a833e59a46ad0ec1",
+    "url": "/static/media/QLogo.1c85c342.png"
+  },
+  {
+    "revision": "c8e42477f6aa310990918758f1eeae3c",
+    "url": "/static/media/background.c8e42477.jpg"
+  }
+]);
